45890652ddc11f745b966f1150ceb0135e17a15b
azizu06 <ab725492@ucf.edu>
2026-05-09 15:43:34 -0400

    init project

--- db/pool.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { Pool } = require("pg");
-
-module.exports = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
