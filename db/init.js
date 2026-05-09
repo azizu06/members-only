d302d32876e63830dadc30e7ff89025b700ff5a1
azizu06 <ab725492@ucf.edu>
2026-05-09 18:21:27 -0400

    Define database schema init script

--- db/init.js
+++ db/init.js
@@ -1,1 +1,35 @@
+require("dotenv").config();
 const { Client } = require("pg");
+
+const createTables = `
+  DROP TABLE IF EXISTS messages
+  DROP TABLE IF EXISTS users
+  
+  CREATE TABLE users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name TEXT,
+    last_name TEXT,
+    username TEXT,
+    password TEXT,
+  )
+
+  CREATE TABLE messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title TEXT,
+    message TEXT,
+    user_id INTEGER REFERENCES users(id)
+  )
+`;
+
+const main = async () => {
+  const client = new Client({
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  await client.connect();
+  await client.query(createTables);
+  await client.end();
+};
+main();
