dc74289bbcd283b899f6d069c2a0177311e31d9d
azizu06 <ab725492@ucf.edu>
2026-05-09 20:44:43 -0400

    Configure Passport sessions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require("node:path");
 const express = require("express");
-const passport = require("passport");
+const passport = require("./config/passport");
 const { Strategy } = require("passport-local");
 const session = require("express-session");
 
@@ -8,7 +8,12 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(session({ secret: "aziz", resave: false, saveUninitialized: false }));
+app.use(session({ secret: "soccer", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
 app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const { Strategy } = require("passport-local");
+const bcrypt = require("bcryptjs");
+const db = require("../db/queries");
+
+passport.use(
+  new Strategy(async (username, password, done) => {
+    try {
+      const user = await db.getUser(username);
+      if (!user) return done(null, false, { message: "Incorrect username" });
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) return done(null, false, { message: "Incorrect password" });
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = db.getUser(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
